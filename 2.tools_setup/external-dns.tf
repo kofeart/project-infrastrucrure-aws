07e6e1472945122c197a6d9586b9c5d239226a94
farrukh90 <farrukhsadykov@gmail.com>
2023-04-27 21:10:57 -0500

    added more roles

--- 2.tools_setup/external-dns.tf
+++ 2.tools_setup/external-dns.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "external-dns-role" {
+  name = "external-dns-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 
 
 module "external-dns-terraform-helm" {
