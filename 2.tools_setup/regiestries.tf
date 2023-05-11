cf717bd4f6edf44d3884c8df5a53b03249f904d3
lucas-020 <luccasa5030@gmail.com>
2023-05-11 19:41:47 +0000

    update regiestries

--- 2.tools_setup/regiestries.tf
+++ 2.tools_setup/regiestries.tf
@@ -46,4 +46,4 @@ resource "aws_iam_policy" "ecr_policy" {
 }
 EOF
 }
-#  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 062967708344.dkr.ecr.us-east-1.amazonaws.com
+
