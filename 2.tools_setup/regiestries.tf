fc8b326435f968f34b714fc27ae3a4bb4e9e6fb2
lucas-020 <luccasa5030@gmail.com>
2023-05-11 17:49:45 +0000

    added regiestries

---
+++ 2.tools_setup/regiestries.tf
@@ -0,0 +1,49 @@
+resource "aws_ecr_repository" "my_repo" {
+  name                 = "artemis"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+resource "aws_ecr_repository" "my-artemis-repo" {
+  name                 = "nodejs"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+resource "aws_ecr_repository" "my-tools-repo" {
+  name                 = "tools"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name   = "ecr-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+#  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 062967708344.dkr.ecr.us-east-1.amazonaws.com
